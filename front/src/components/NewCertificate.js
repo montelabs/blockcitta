6fa2199eff26e3f7ebe1005a902a18bc2d773eaf
Fynn <enriquefynn@gmail.com>
2017-09-02 13:57:21 +0200

    fields

--- front/src/components/NewCertificate.js
+++ front/src/components/NewCertificate.js
@@ -1,17 +1,100 @@
 import React, { Component } from 'react'
 
+import {GridList, GridTile} from 'material-ui/GridList';
+
+import RaisedButton from 'material-ui/RaisedButton';
+import TextField from 'material-ui/TextField';
+import SelectField from 'material-ui/SelectField';
+import MenuItem from 'material-ui/MenuItem';
+import DatePicker from 'material-ui/DatePicker';
+
 class NewCertificate extends Component {
   constructor(props){
     super(props)
     this.state = {
+      sex: null,
+      fullname: null
     }
+  }
+
+  static gridListStyle = {
+    marginTop:10
+  };
+
+  handleFullNameChange = (event, value) => {
+    console.log(value);
+  }
+
+  handleSexChange = (event, _, value) => {
+    this.setState({sex: value});
+  }
+
+  formatDate = (date) => {
+    console.log(date.getMonth())
+    return `${date.getDate()}.${date.getMonth()+1}.${date.getFullYear()}`;
   }
 
   render(){
     return(
-      <div>
-        New
-      </div>
+      <form onSubmit={this.handleOnSubmit} >
+        <div>
+          <GridList
+            style={NewCertificate.gridListStyle}
+            cellHeight={'auto'}
+            cols={3}
+          >
+            <GridTile>
+              <TextField
+                fullWidth={true}
+                name='fullname'
+                floatingLabelText='Nome Completto'
+                onChange={this.handleFullNameChange}
+              />
+            </GridTile>
+            <GridTile>
+              <SelectField
+                floatingLabelText='Sesso'
+                value={this.state.sex}
+                onChange={this.handleSexChange}
+              >
+                <MenuItem value={'M'} primaryText='M'/>
+                <MenuItem value={'F'} primaryText='F'/>
+              </SelectField>
+            </GridTile>
+            <GridTile>
+              <DatePicker
+                hintText='Data di nascita'
+                container='inline'
+                formatDate={this.formatDate}
+              />
+            </GridTile>
+          </GridList>
+
+          <GridList
+            style={NewCertificate.gridListStyle}
+            cellHeight={'auto'}
+            cols={2}
+          >
+            <GridTile>
+              <TextField
+                fullWidth={true}
+                name='fullname'
+                floatingLabelText='Indirizzo (via, CAP e località)'
+                onChange={this.handleAddress}
+              />
+            </GridTile>
+            <GridTile>
+              <TextField
+                fullWidth={true}
+                name='e-mail'
+                floatingLabelText='e-mail'
+                onChange={this.handleAddress}
+              />
+            </GridTile>
+          </GridList>
+        </div>
+        <RaisedButton type='submit' label='Invia' primary />
+      </form>
     )
   }
 }
