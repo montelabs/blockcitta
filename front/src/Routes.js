1f3bb9a72bec49437bb004e0998323180a20d0e8
Leonardo Alt <leonardoaltt@gmail.com>
2017-09-02 12:48:48 +0200

    Bugged

--- front/src/Routes.js
+++ front/src/Routes.js
@@ -4,12 +4,14 @@ import {Route, IndexRedirect} from 'react-router';
 
 import Master from 'components/Master';
 import ResidenceCertificate from 'components/ResidenceCertificate';
+import PuntoCittaAdm from 'components/PuntoCittaAdm';
 
 const Routes = (
   <Route path='/' component={Master}> 
     <Route path='certificati'>
       <Route path=':type/:newOrVerify' component={ResidenceCertificate} />
     </Route>
+    <Route path='richieste' component={PuntoCittaAdm} />
   </Route>
 );
 
