0b547a321c9d11bd42f5da6c108e289e41dbc186
Leonardo Alt <leonardoaltt@gmail.com>
2017-09-02 14:49:36 +0200

    Proposal list UI

--- front/src/components/Proposal.js
+++ front/src/components/Proposal.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react'
 import {Card, CardActions, CardHeader, CardText} from 'material-ui/Card';
+import RaisedButton from 'material-ui/RaisedButton';
+
+import lodash from 'lodash';
 
 class Proposal extends Component {
   constructor(props) {
     super(props);
-    console.log(props);
+    this.state = {
+      isExpanded: false
+    }
   }
 
   ReqType = () => {
@@ -22,23 +27,68 @@ class Proposal extends Component {
     return _t;
   }
 
+  ResId = () => {
+    var _i = 'ID Residente: ' + this.props.proposal.resId.toString();
+    return _i;
+  }
+
+  ActionButtons = () => {
+    if (!this.state.isExpanded)
+      return null;
+
+    return (
+      <div
+        style={{display: 'flex', flexDirection: 'row', justifyContent: 'center'}}
+      >
+        <RaisedButton
+          style={{margin: 20}}
+          label="Falso"
+          secondary={true}
+          onTouchTap={() => this.rejectProposal()}
+        />
+        <RaisedButton
+          style={{margin: 20}}
+          label="Tutto OK"
+          primary={true}
+          onTouchTap={() => this.resolveProposal()}
+        />
+      </div>
+    );
+  }
+
+  rejectProposal = () => {
+    console.log('Reject');
+  }
+
+  resolveProposal = () => {
+    console.log('Resolve');
+  }
+
   render() {
     if (this.props.proposal.reqStatus.toString() !== '0')
       return null;
 
     return (
-      <Card style={{marginRight: 80, marginBottom: 20}}>
+      <Card style={{marginRight: 80, marginBottom: 20}}
+            onExpandChange={lodash.debounce(this.onExpand, 150)}
+            expanded={this.state.isExpanded}
+      >
         <CardHeader
           actAsExpander={true}
           showExpandableButton={true}
           title={this.ReqTitle()}
+          subtitle={this.ResId()}
         />
         <CardText expandable={true}>
-          {this.props.proposal.resId.toString()}
-          {this.props.proposal.dataHash.toString()}
+          Dati: {this.props.proposal.dataHash.toString()}
         </CardText>
+        <this.ActionButtons />
       </Card>
     );
+  }
+
+  onExpand = () => {
+    this.setState(previousState => ({isExpanded: !previousState.isExpanded}));
   }
 
 }
